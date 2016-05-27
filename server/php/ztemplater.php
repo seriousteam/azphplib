2aca2bc845cd80d16fa7d16d86e3262d9f8ab6d1
Alexander Nedosekin <anedosekin@gmail.com>
2016-05-27 18:33:37 +0400

    ztemplater - js reload function

--- server/php/ztemplater.php
+++ server/php/ztemplater.php
@@ -12,7 +12,7 @@ function main_address() {
 	else
 		return null;
 	
-	$addr['plane'] = @$_REQUEST['plane'] ? TRUE : FALSE;
+	$addr['flat'] = @$_REQUEST['flat'] ? TRUE : FALSE;
 
 	$i=0;
 	while(@$_REQUEST["pk$i"]) {
@@ -49,9 +49,11 @@ function dispatch_template2($addr, $cmd, $args) {
 	$data_func = $functions[$func_name]['data'];
 	if($addr) {
 		//generate and send data
-		echo json_encode(
-			$data_func($addr, $cmd, $args/*, new smap(null, $_REQUEST)*/)
-		);
+		echo json_encode([ 'Q1'=>[], 'info' => [] ]);
+
+		//echo json_encode(
+		//	$data_func($addr, $cmd, $args/*, new smap(null, $_REQUEST)*/)
+		//);
 	} else {
 		//generate data		
 		$data = $data_func(null, $cmd, $args/*, new smap(null, $_REQUEST)*/);		
@@ -131,51 +133,48 @@ $functions['_main_']['data'] = function($addr, $cmd, $args = null, $params = nul
 	//////////////Template variable/////////////
 	////////////////////////////////////////////
 	$main_query = 'Q1';
-	$query_map = [
-		'Q1' => 'Q1',
-		'Q2' => 'Q1.Q2',
-		'Q3' => 'Q1.Q2.Q3'
-	];	
 	$query_tree = [
 	'Q1' => [
-		'table' => 'enrf_stages',
+		'table' => 'country',
 		'select' => [
-			'syrecordidw' => ['alias' => 'a__syrecordidw'],
-			'enf_moneyw' => ['alias' => 'a__enf_moneyw'],
+			'country_id' => ['alias' => 'a__country_id'],
+			'country' => ['alias' => 'a__country'],
 			'Q2' => [
-				'table' => 'enrf_expertises',
+				'table' => 'city',
 				'select' => [
-					'syrecordidw' => ['alias' => 'a__syrecordidw'],
-					'enf_ratew' => ['alias' => 'a__enf_rate'],
-					'enrel_taskw' => ['alias' => 'a__enrel_taskw'],
+					'city_id' => ['alias' => 'a__city_id'],
+					'city' => ['alias' => 'a__city'],
+					'country_id' => ['alias' => 'a__country_id'],
 					'Q3' => [
-						'table' => 'enrf_expertise_items',
+						'table' => 'address',
 						'select' => [
-							'syrecordidw' => ['alias' => 'a__syrecordidw'],
-							'enf_paramw' => ['alias' => 'a__enf_paramw'],
-							'enrel_contw' => ['alias' => 'a__enrel_contw']
+							'address_id' => ['alias' => 'a__address_id'],
+							'address' => ['alias' => 'a__address'],
+							'address2' => ['alias' => 'a__address2'],
+							'city_id' => ['alias' => 'a__city_id']
 						],
-						'from' => 'FROM enrf_expertise_items WHERE enrel_contw=ext.syrecordidw',
-						'ext' => ['Q2.syrecordidw']
+						'from' => 'address WHERE city_id=ext.city_id',
+						'ext' => ['Q2.city_id']
 					]
 				],
-				'from' => 'FROM enrf_expertises WHERE enrel_taskw = ext.syrecordidw',
-				'ext' => ['Q1.syrecordidw']
+				'from' => 'city WHERE country_id=ext.country_id',
+				'ext' => ['Q1.country_id']
 			]
 		],
-		'from' => 'FROM enrf_stages WHERE enf_moneyw IS NOT NULL AND enf_datew IS NOT NULL ORDER BY enf_datew LIMIT 10'		
+		'from' => 'country LIMIT 10'		
 	]	
 	];
 	///////////////////////////////////////////
 	////////////Template environment///////////
 	///////////////////////////////////////////
 	$addr = $addr ?: ['get' => $main_query];
-
-	$queryname = $addr['get'];
-	$path = explode('.', $query_map[ $queryname ]);
-	$query = $query_tree[ array_shift($path) ];	
+	
+	$path = explode('.', $addr['get']);
+	$queryname = array_shift($path);
+	$query = $query_tree[ $queryname ];	
 	foreach($path as $q) {
 		$query = $query["select"][$q];
+		$queryname = $q;
 	}
 
 	//collect pk filter
@@ -196,15 +195,16 @@ $functions['_main_']['data'] = function($addr, $cmd, $args = null, $params = nul
 	//construct query
 	function buildSQL($query, $fields) {
 		$fields = array_map(function($field, $name) {
+			$name = strtolower($name);
 			if(@$field['select']) {
 				$select = buildSQL( $field, $field["select"] );
 				return "( $select ) AS ARRAY $name";
 			}
 			return "a.$name AS {$field['alias']}";
 		}, $fields, array_keys($fields) );
-		return 'SELECT '.implode(', ',$fields)." {$query['from']}";
+		return 'SELECT '.implode(', ',$fields)." FROM {$query['from']}";
 	};
-
+    
 	$qcmd = merge_queries( buildSQL($query, $fields), 
 		$cmd, $args, $requested_offset, $requested_limit, $page_limit);
 
@@ -223,19 +223,20 @@ $functions['_main_']['data'] = function($addr, $cmd, $args = null, $params = nul
 	//generate data structure	
 	$counters = new stdClass;
 	$statements = new stdClass;
-	function collectSQL($queryname, $fields, &$rowset, &$data,
+	function collectSQL($sqlqueryname, $fields, &$rowset, &$data,
 		&$counters, &$statements, $info = null) {
 		$info = $info ?: @$rowset->exInfo;
 		foreach(
 		with_loop_info($rowset, 
-			$counters->{$queryname}, 
-			$statements->{$queryname} = $info) as $d) {
+			$counters->{$sqlqueryname}, 
+			$statements->{$sqlqueryname} = $info) as $d) {
 			$row = [];
 			foreach($fields as $name=>$field) {
+				$sqlname = strtolower($name);
 				if(@$field['select']) {
-					$row[$name] = [];
-					collectSQL( $name, $field['select'], $d->{$name}, $row[$name], 
-						$counters, $statements, $d->subselect_info($name) );
+					$row[$name] = [];					
+					collectSQL( $sqlname, $field['select'], $d->{$sqlname}, $row[$name], 
+						$counters, $statements, $d->subselect_info($sqlname) );
 				} else {
 					$row[$name] = (string)$d->ns($field['alias']);
 				}
@@ -243,12 +244,13 @@ $functions['_main_']['data'] = function($addr, $cmd, $args = null, $params = nul
 			$data[] = $row;
 		}
 	};
+	$sqlqueryname = strtolower($queryname);
 	$response[ $queryname ] = [];
 	$response[ 'info' ] = [];
+	
+	$rowsets[ $sqlqueryname ] = process_query($qcmd, $args);
 
-	$rowsets[ $queryname ] = process_query($qcmd, $args);
-
-	collectSQL( $queryname, $fields, $rowsets[ $queryname ], $response[ $queryname ], 
+	collectSQL( $sqlqueryname, $fields, $rowsets[ $sqlqueryname ], $response[ $queryname ], 
 		$counters, $statements );	
 	/*
 	PHP
@@ -283,111 +285,267 @@ $functions['_main_']['template'] = function($data, $cmd = null, $args = null, $p
   <script type="text/javascript" 
   src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-resource.js"></script>
   <script>
-	function linkData(data,info,depth,parentparams) {
-		parentparams = parentparams || [];
-		data.$ = {
-			dst : data,
-			src : { 
-				get : depth[ depth.length-1 ]
+  	/*
+			Generated structure:
+			--------------------------
+			Array:
+			$ : new Query { ( Service data )
+				parent : Row | Array
+				path
 			}
+			[
+				{ ..Row.. }
+				{ ..Row.. }
+				{ ..Row.. }
+			]
+			
+			--------------------------
+			Row:
+			{
+				$ : new Row { ( Service data )
+					parent : Array
+					path
+				}
+				field1 : { ..Field.. },
+				field2 : { ..Field.. }
+			}
+			
+			--------------------------
+			Field:
+			{
+				$ : new Field { ( Service data )
+					parent : Row
+					path
+				}
+				value : 'val',
+				pk : true | false
+			}
+			
+			--------------------------
+	*/
+
+	/*
+  		Address
+		{
+			get : Q1 -> get : Q1
+			pk : [val1, val2,...] -> pk0 : val1, pk1 : val2
+			pp : [val1, val2,...] -> pp0 : val1, pp1 : val2
+			name : [field1, field2,...] -> name0 : field1, name1 : field2
 		}
-		data.$.src.pp = parentparams.slice();	
-		var pk = info[ depth.join('.') ].pk;
-		data.forEach(function(row) {
-			var descr = {
-				dst : row,
-				src : angular.copy(data.$.src)
-			}
-			for(var fldname in row) {
-				if(!angular.isArray(row[fldname])) {
-					row[fldname] = { 
-						value : row[fldname]
-					}
-				}
-			}
-			descr.src.pk = [];
-			pk.forEach(function(pkname,i) {
-				if(!row[pkname]) console.log("Error! Table info incorrect through bad pk definition");
-				descr.src.pk.push( row[pkname].value );
-				row[pkname].pk = true;
-			})				
-			for(var fldname in row) {
-				var infoname = depth.join('.')+'.'+fldname;
-				var field = row[fldname];
-				if(angular.isArray(field)) {					
-					linkData( field, info, depth.concat(fldname), 
-						parentparams.concat(pk.map(function(pkname) {
-							return row[pkname].value
-						}))
-					);				
-				} else {
-					if(info[infoname] && info[infoname].requery) {
-						//full requery
-						field.dst = data;
-						field.src = angular.copy(data.$.src);
-					} else {
-						field.dst = row;				
-						field.src = angular.copy(descr.src);
-						//row requery for pk
-						if(!field.pk) {
-							field.src.name = [ fldname ];
-						}						
-					}					
-				}
-			}
-			row.$ = descr;
-		})
-	}
-	function linkScope(scope,queries) {
+  	*/
+  	function isQuery(elem)	{ return angular.isArray(elem) }
+  	function isRow(elem) { return elem.$.parent_array }
+  	function isField(elem) { return !angular.isArray(elem) && elem.$.parent_row }
+  	function getPath(elem, path/*=null*/) {
+  		if(elem.$.parent_array) {
+  			//row
+  			return getPath(elem.$.parent_array, path);
+  		} else
+  		if(elem.$.parent_row) {
+  			//field or child-array
+  			return getPath(elem.$.parent_row, elem.$.name + (path?('.'+path):''));
+  		} else {
+  			//root
+  			return elem.$.name + (path?('.'+path):'');
+  		}  		
+  	}
+  	function getItsQuery(elem) {
+  		/* 
+  		*	returns query of field and row, parent query for query, and null for root query 
+  		*/
+  		return elem.$.parent_array || (elem.$.parent_row && elem.$.parent_row.$.parent_array) || null;  		
+  	}
+  	function getClosestQuery(elem) {
+  		/* 
+  		*	returns query of field and row, and query itself for query
+  		*/
+  		return isQuery(elem) ? elem : (elem.$.parent_array || elem.$.parent_row.$.parent_array)
+  	}
+  	function getRow(elem) {
+  		return isRow(elem) ? elem : elem.$.parent_row;
+  	}
+  	function getPK(elem, info) {
+  		/*  
+  		*	['val1','val2',...]
+  		*	Q1.Q2.Q3.row.field - pk of Q3
+		*	Q1.Q2.Q3.row - pk of Q3
+		*	Q1.Q2.Q3 - pk of Q2
+		*	Q1 - null 
+  		*/
+  		var query = getItsQuery( elem );
+  		if(!query) return null;
+  		var pk = info[ getPath(  query ) ].pk || [];
+  		var row = getRow( elem );
+  		return pk.map(function(name) { return row[name].value })
+  	}
+  	function getPP(elem,info) {
+  		/* 
+  		*	['val','val','val',...]
+  		*	Q1.Q2.Q3.row.field - pk of Q1, Q2
+  		*	Q1.Q2.Q3.row - pk of Q1, Q2
+  		*	Q1.Q2.Q3 - pk of Q1
+  		*	Q1.Q2 - []
+  		*	Q1 - []
+  		*/
+  		var pp = [];
+  		while(elem = getItsQuery(elem)) {
+  			var pk = getPK(elem,info);
+  			if(pk) {
+  				for(var i=pk.length-1;i>=0;i--) {
+	  				pp.unshift(pk[i]);
+	  			}
+  			}	  		
+  		}
+  		return pp;
+  	}  	
+  	function linkField(name, row) {//repeatcall-safe ${name, parent_row}
+  		row[name] = {
+  			$ : { name : name, parent_row : row },
+  			value : row[name].value || row[name]
+  		}
+  	}
+  	function linkRow(row, query) {//repeatcall-safe ${parent_array}
+  		delete row.$;
+  		for(var name in row) {
+  			if(angular.isArray(row[name])) {
+  				linkQuery(name, row[name], row);
+  			} else {
+  				linkField(name, row);
+  			}
+  		}
+  		row.$ = { parent_array : query	}
+  	}
+  	function linkQuery(name, array, row) {//repeatcall-safe ${name, parent_row}
+  		array.$ = {	name : name, parent_row : row }
+  		array.forEach(function(r) { linkRow(r, array) })
+  	}
+	function linkScope(queries,scope) {
   		queries.forEach(function(query) {
-  			linkData(scope[query], scope.const_info, [query]);
+  			linkQuery(query, scope[query]);
   		})
   	}
-  	function encodeResource(src) {
+  	function encodeAddress(src) {
+  		var rez = {};
   		for(var name in src) {
   			if(angular.isArray(src[name])) {
   				src[name].forEach(function(value,i) {
-  					src[name+i] = value;
+  					rez[name+i] = value;
   				});
-  				delete src[name];
+  			} else if(src[name]!==undefined) {
+  				rez[name] = src[name];
   			}
   		}
-  		return src;
+  		return rez;
+  	}
+  	function getReload(elem, info) {
+  		var src = { get : getPath( getClosestQuery(elem) ), pp : getPP(elem,info) };
+  		if(!angular.isArray(elem)) {
+  			src.pk = getPK(elem,info);
+  			if(!elem.$.parent_array) {
+  				src.name = [elem.$.name];
+  			}
+  		}
+  		//return encodeAddress(src);
+  		return { 
+  			src : encodeAddress(src), 
+  			ok : function(response) {
+  				/* Structure of response
+				*	{ 
+				*		"info" : [],
+				*		"Q1" : [
+				*			{"f1" : "value1",...},
+				*			...
+				*		]
+				*	}
+  				*/
+  				var rawdata = [];
+  				for(var i in response.data) {
+  					if(i !== 'info') { rawdata = response.data[i] }
+  				}
+  				if(isQuery(elem)) {
+  					//empty array if empty response
+  					elem.splice(0,elem.length);
+  					if(rawdata.length) {
+  						for(var i=0;i<rawdata.length;++i) {
+	  						elem.push(rawdata[i]);
+	  					}
+	  					linkQuery(elem.$.name, elem, elem.$.parent_row);
+  					}  					
+  					return;
+  				}
+  				if(isRow(elem)) {
+  					var idx = elem.$.parent_array.indexOf(elem);  					
+  					if(rawdata.length) {
+  						elem.$.parent_array.splice( idx, 1, rawdata[0]);
+  						linkRow(elem.$.parent_array[idx], elem.$.parent_array);
+  					} else {
+  					//delete row
+  						elem.$.parent_array.splice( idx, 1 );
+  					}
+  					return;
+  				}
+  				if(isField(elem)) {
+  					if(rawdata.length) {
+  						var name = elem.$.name;
+  						var row = elem.$.parent_row;
+  						row[name] = rawdata[0][name];
+  						linkField(name, row);
+  					} else {
+  					//delete row
+  						var array = elem.$.parent_row.$.parent_array;
+  						array.splice( array.indexOf(elem.$.parent_row), 1 );
+  					}
+  				}
+  			}
+  		};
+  	}
+  	function pushDataIntoScope(elem,data) {
+  		//if() {
+
+  		//}
   	}
   </script>
 <script>
 var heart = angular.module('heart',['ngResource']);
-heart.controller('main', function($scope, $resource) {
-var Server = $resource(location.href);
+heart.controller('main', function($scope, $http, $resource) {
+//var Server = $resource(location.href);
 $scope.reload = function(elem) {
-	console.log(elem);
-	var descr = elem.$ ? elem.$ : elem;
-	Server.query( encodeResource(descr.src) );
+	var addr = getReload(elem, $scope.const_info );
+	$http.get(location.href, { params : addr.src })
+	.then( addr.ok,
+	function(data) {
+
+	})
+	//console.log(elem);
+	//var descr = elem.$ ? elem.$ : elem;
+	//Server.query( xreload(elem, $scope.const_info ) );
 	//elem.$reload({})
-	/*
-	var result = Server.query({ query:q }, function() {
-		$scope[q] = result;
-	});*/
+	//var result = Server.query( getAddress(elem, $scope.const_info ), 
+	//	function() {
+	//
+	//		//$scope[q] = result;
+	//	});
+	//elem.$
 }
 //What You Requested Is What You Get WYRIWYG
 $scope.const_info = {
-	"Q1" : { pk : ["syrecordidw"], plane: true },
-	"Q1.Q2" : {	pk : ["syrecordidw"], plane: true },
-	"Q1.Q2.Q3" : {pk : ["syrecordidw"], plane: true },
-	"Q1.syrecordidw" : {},
-	"Q1.enrel_taskw" : {},
-	"Q1.Q2.syrecordidw" : {},
-	"Q1.max" : { requery : true },
-	"Q1.Q2.sum" : { requery : true },
-	"Q1.rel1.rel2" : { fk : ["pk1","pk2"] },
-	"Q1.rel1" : { fk : ["pk"] }
+	"Q1" : { pk : ["country_id"], flat: true }
+	,"Q1.Q2" : {	pk : ["city_id"], flat: true }
+	,"Q1.Q2.Q3" : { pk : ["address_id"], flat: true }
+	//"Q1.syrecordidw" : {},
+	//"Q1.enrel_taskw" : {},
+	//"Q1.Q2.syrecordidw" : {},
+	//"Q1.max" : { src : "Q1" },
+	//"Q1.Q2.sum" : { src: "Q1.Q2" },
+	//"Q1.rel1.rel2" : { fk : ["pk1","pk2"] },
+	//"Q1.rel1" : { fk : ["pk"] }
 }
 <?php
 	foreach($data as $k=>$v) {
 		echo "\$scope.$k=".json_encode($v, JSON_PRETTY_PRINT).";".PHP_EOL;
 	}
 ?>
-linkScope($scope, ['Q1']);
+linkScope(['Q1'], $scope);
 		/*
 		$scope.info = {
 			"Q1" : { offset : 30 },
@@ -442,26 +600,22 @@ linkScope($scope, ['Q1']);
 </head>
 <body ng-app=heart ng-controller=main>
 	<div>Z template instance</div>
-	<div ng-repeat="row in Q1">
-		<button ng-click="reload(row.syrecordidw)">reload Q1.row.syrecordidw</button>
-		<button ng-click="reload(row.enf_moneyw)">reload Q1.row.enf_moneyw</button>
-		<button ng-click="reload(row.max)">reload Q1.max</button>
-		{{row.syrecordidw.value}} : {{row.enrel_taskw.value}} : {{row.enf_moneyw.value}}
-		<input ng-model="row.f1.value">
-		<div ng-repeat="row2 in row.Q2">{{row.syrecordidw.value}}-{{row2.syrecordidw.value}}
-			<button ng-click="reload(row2)">subreload</button>
-			<button ng-click="reload(row2.syrecordidw)">subreload syrecordidw</button>
-			<button ng-click="reload(row2.enf_ratew)">subreload enf_ratew</button>
-			<button ng-click="reload(row2.sum)">subreload sum</button>
-			<div ng-repeat="row3 in row2.Q3">
-				{{row3.enf_paramw.value}}
-				<button ng-click="reload(row3)">reload Q3.row</button>
-				<button ng-click="reload(row3.enf_paramw)">reload Q3.row.enf_paramw</button>
+	<div ng-repeat="row in Q1" style="border:1px solid black;padding:0.5em;margin:1em;border-radius:1em">
+		<button ng-click="reload(row.country_id)">row.country_id</button>
+		{{row.country.value}}
+		<input ng-model="row.country.value">
+		<div ng-repeat="row2 in row.Q2" style="border-radius:1em;background:rgba(255,255,0,0.2);padding:0.5em;margin:0.5em;box-shadow:0 0 5px rgba(0,0,0,0.1)">{{row2.city.value}}({{row.country.value}}){{row2.city_id.value}}
+			<button ng-click="reload(row2)">Q2 row reload</button>
+			<button ng-click="reload(row2.city_id)">row2.city_id</button>
+			<div ng-repeat="row3 in row2.Q3" style="padding:0.5em;margin:0.5em;border-radius:1em;background:rgba(0,0,255,0.1)">
+				{{row3.address.value}}
+				<button ng-click="reload(row3)">Q3 row reload  </button>
+				<button ng-click="reload(row3.address)">Q3.address</button>
 			</div>
-			<button ng-click="reload(row2.Q3)">subreload Q3</button>
+			<button ng-click="reload(row2.Q3)">Q3 reload </button>
 		</div>
-		<button ng-click="reload(row)">reload Q1.row</button>
-		<button ng-click="reload(row.Q2)">reload Q2</button>
+		<button ng-click="reload(row)">Q1 row reload</button>
+		<button ng-click="reload(row.Q2)">Q2 reload</button>
 	</div>
 	Sum : {{id}}
 	<button type=button ng-click="reload(Q1)">fullreload</button>
