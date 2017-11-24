173ea5a5903a2f9c23574b7f528d9aa06ef4a7ca
andy <andy7d0@gmail.com>
2017-11-24 20:50:43 +0300

    deduplicator-patch

--- server/php/deduplicator.php
+++ server/php/deduplicator.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/envars.php';
 
 //if same request in progress - wait first one and return it's result
+// $func($key) take key, output result and return it's mime type (or null, if default mimen type used)
 function deduplicate_request(
 	$zone, $key, $user,
 	$func
@@ -17,13 +18,17 @@ function deduplicate_request(
 	if(flock($f,LOCK_EX + LOCK_NB)) {
 		//main thread
 		ftruncate($f,0);
-		$result = $func($key); //call generation function
+		ob_start();
+		$mime = $func($key); //call generation function
+		$result = ob_get_clean();
 		fwrite($f, $result);
 		flock($f, LOCK_UN);
 		unlink($fn);
+		if($mime) @header("Content-type: $mime");
 		echo $result;
 	} else {
 		if(flock($f, LOCK_SH)){
+			if($mime) @header("Content-type: $mime");
 			fpassthru($f);
 			flock($f, LOCK_UN);
 		}
